09abfb8cdd89880f37ab15f635d8cc53a6b6c7dc
Noel Arcenio Cortes <156055746+NoelCortesP@users.noreply.github.com>
2024-11-19 19:14:18 +0800

    Update Test.c

--- Test.c
+++ Test.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdlib.h>
 
+int VerificationUsernameIFValid(char *username){
+
+    int len = strlen(username);
+
+
+    if(len < 3 || len >= 20){
+        puts("\nUsername should atleast be 3 characters and not exceeding 20.\r");
+        return 0;
+    }
+
+    for(int i = 0; i < len; i++){
+        if(ispunct(username[i])){
+        puts("\nNo special characters such as \"@, #, _, !\".");
+        return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main(){
+
+    char command[50], username[50];
+    int validationStatus;
+
+    do{
+    printf("\nEnter username: ");
+    fgets(username, sizeof(username), stdin);
+    username[strcspn(username, "\n")] = '\0';
+
+    validationStatus = VerificationUsernameIFValid(username);
+    }while(validationStatus != 1);
+
+
+    if(validationStatus == 1){
+        printf("\nValid Username.");
+    }
+
+    abort();
+}
